645522679c647f4ca62d31b323610ca32d12c5e4
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2021-05-21 16:03:50 -0700

    created basic nav layout

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,13 @@
 <template>
-  <div></div>
+  <nav>
+      <a href=""><p>Outdoor Vibrations</p></a>
+      <ul>
+          <a href=""><li>Home</li></a>
+          <a href=""><li>About</li></a>
+          <a href=""><li>Blog</li></a>
+      </ul>
+      <a href=""><p>Subscribe</p></a>
+  </nav>
 </template>
 
 <script>
@@ -8,6 +16,22 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+    * {
+        text-decoration: none;
+    }
 
+    nav {
+        display: flex;
+        justify-content: space-evenly;
+    }
+
+    ul {
+        display: flex;
+    }
+
+    li {
+        margin-left: 42px;
+        list-style: none;
+    }
 </style>
