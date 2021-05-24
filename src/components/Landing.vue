1a4334d4e98245dbf37327340062c30e86ac746e
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2021-05-24 08:56:17 -0700

    add main content

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -3,6 +3,11 @@
         <div>
             <h1>Connecting to Nature</h1>
             <h2>Have a love for all things that are outdoors?</h2>
+            <a href="">Join Now</a>
+            <a href="">Read Blog</a>
+        </div>
+        <div>
+            <img src="../assets/camping.png" alt="">
         </div>
     </main>
 </template>
